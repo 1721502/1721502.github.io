aabd4801d7ae350950cc93f733758eea209fb57a
1721502 <87455090+1721502@users.noreply.github.com>
2021-07-15 10:24:09 +0900

    初期コミット

---
+++ week13/js/sample.js
@@ -0,0 +1,68 @@
+var labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+var data = {
+    labels: labels,
+    datasets: [{
+        label: '初めてのグラフ',
+        borderColar: 'rgb(225, 100, 130)',  
+        backgroundColar:  'rgb(150, 25, 150)',
+        data: [0, 10, 15, 3, 20, 60, 10],
+    }]
+};
+
+var config = {
+    type: 'line',
+    data,
+    options: {},
+};
+
+var lineChart = new Chart(
+    document.getElementById('linechart'),
+    config,
+);
+
+
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 1
+    }]
+};
+const barConfig = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+};
+
+var barChart = new Chart(
+    document.getElementById('barchart'),
+    barConfig,
+    );
+ 
